55aba1bd4a1fea4d086ede70079732b502488a22
Ida Delphine <idadelm@gmail.com>
2021-11-30 22:37:52 +0100

    Add Cursor component

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './global';
 import { theme } from './theme';
 import Home from './pages/Home';
+import Cursor from './components/Cursor';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyles />
+        <Cursor />
         <div className="App">
+          
           <Home />
         </div>
     </>
