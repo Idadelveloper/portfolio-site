24d2fc1c0f3dfc4126589b2674da30af88acd8fc
Ida Delphine <idadelm@gmail.com>
2021-12-01 23:33:29 +0100

    Add homepage portfolio component

---
+++ client/src/components/HomePortfolio/Portfolio.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import portfolio from '../../assets/image-1.jpg'
+import './Portfolio.css'
+import '../../index.css'
+
+export default function Portfolio() {
+    return (
+        <div style={{cursor: "pointer"}} className="portfo">
+            <div className="portfolio grid grid-cols-1 md:grid-cols-2">
+                <div className="text">
+                    <h1 className="text1">Portfo<br />lio</h1>
+                    <h2 className="text2">Portfolio</h2>
+                </div>
+                <h1 className="text3">Port<br />folio</h1>
+                <h2 className="text4">Portfolio</h2>
+                <img src={portfolio} alt=""className="image" />
+                
+            </div>
+        </div>
+    )
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import Services from '../components/Services/Services';
+import Portfolio from '../components/HomePortfolio/Portfolio';
 
 export default function Home() {
     return (
         <div>
             <Banner />
             <Services />
+            <Portfolio />
         </div>
     )
 }
