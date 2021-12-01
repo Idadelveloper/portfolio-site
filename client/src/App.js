3b8785b7ee96ac554789c29f96a7e54c8f0dc1ae
Ida Delphine <idadelm@gmail.com>
2021-12-01 14:57:11 +0100

    Adjust banner

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyles />
-        <Cursor />
+        {/* <Cursor /> */}
         <div className="App">
           
           <Home />
