d9c9017ed24dc7868c1840df138cef878151f567
Ida Delphine <idadelm@gmail.com>
2021-12-02 00:18:21 +0100

    Add homepage resume component

---
+++ client/src/components/HomeResume/Resume.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import resume from '../../assets/image-2.jpg'
+import '../../index.css'
+import './Resume.css'
+
+export default function Resume() {
+    return (
+        <div style={{cursor: "pointer"}} className="resume">
+            <div className="grid grid-cols-1 md:grid-cols-2">
+                
+                <h1 className="resume-text3">Resu<br />me</h1>
+                <h2 className="resume-text4">Resume</h2>
+                <img src={resume} alt=""className="image" />
+                <div className="resume-text">
+                    <h1 className="resume-text1">Resu<br />me</h1>
+                    <h2 className="resume-text2">Resume</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../components/Banner';
 import Services from '../components/Services/Services';
 import Portfolio from '../components/HomePortfolio/Portfolio';
+import Resume from '../components/HomeResume/Resume';
 
 export default function Home() {
     return (
@@ -9,6 +10,7 @@ export default function Home() {
             <Banner />
             <Services />
             <Portfolio />
+            <Resume />
         </div>
     )
 }
