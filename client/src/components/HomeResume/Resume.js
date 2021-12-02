380f0c33265287c70eca3a6dee8b7421759aa0bf
Ida Delphine <idadelm@gmail.com>
2021-12-02 11:38:19 +0100

    Adjusted styles

--- client/src/components/HomeResume/Resume.js
+++ client/src/components/HomeResume/Resume.js
@@ -6,7 +6,7 @@ import './Resume.css'
 export default function Resume() {
     return (
         <div style={{cursor: "pointer"}} className="resume">
-            <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="res grid grid-cols-1 md:grid-cols-2">
                 
                 <h1 className="resume-text3">Resu<br />me</h1>
                 <h2 className="resume-text4">Resume</h2>
