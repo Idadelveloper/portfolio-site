a5e411b024de8adc4e9666172f4005598bd821ad
Ida Delphine <idadelm@gmail.com>
2021-12-04 11:50:03 +0100

    Add about component

--- client/src/components/MyAbout.js
+++ client/src/components/MyAbout.js
@@ -4,7 +4,17 @@ import './MyAbout.css'
 export default function MyAbout() {
     return (
         <div>
-            
+            <div className="about">
+                <div className="circle circle-a"></div>
+                <div className="circle circle-b"></div>
+                <div className="circle circle-c"></div>
+                <h1 className="about-title">About</h1>
+                <div className="about-content">
+                    <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin et erat sit amet facilisis. Donec risus eros, ultricies eu eleifend nec, dapibus sollicitudin quam. Ut dignissim neque erat, aliquam consectetur orci finibus at. In mattis est ac ultricies porta. In ac elit felis. Quisque auctor, urna a facilisis ultricies, libero leo aliquet justo, ac mattis nunc nunc ut nulla. Duis ornare nunc a neque lobortis eleifend. Aenean vestibulum semper elit, sed interdum dolor consectetur vitae. Morbi tristique a nisi in imperdiet.</p></div>
+                    <div><p>Praesent sit amet dapibus purus. Sed at sapien rutrum, rutrum velit quis, consectetur metus. Maecenas porttitor efficitur dolor, vitae tempor velit rhoncus eget. Nunc ut nisi sed elit maximus dignissim ut tincidunt magna. Etiam in metus leo. Integer condimentum, turpis id tincidunt vehicula, massa nisl convallis nibh, a mollis augue ipsum quis turpis. Nunc pulvinar velit vitae pulvinar sodales. Fusce imperdiet felis ac sem malesuada, non egestas est consectetur. Aenean sit amet porttitor arcu. Suspendisse et nunc sed diam tincidunt bibendum. Vivamus facilisis mi purus. Integer vitae nisi in magna efficitur vehicula vel vel purus. Morbi ac lacus congue, ultricies dolor sit amet, tincidunt enim. Vestibulum at condimentum sem. Proin non sollicitudin lacus, vitae rhoncus mi. Mauris nulla nulla, venenatis a sapien id, tempus commodo lectus.</p> </div>
+                    <div><p>Quisque porttitor est a malesuada porttitor. Sed ultricies risus ut leo venenatis molestie. Nam ultricies pellentesque tristique. Donec aliquet volutpat convallis. Cras vestibulum hendrerit orci, nec iaculis quam accumsan vitae. In cursus augue pellentesque nisl dapibus, ut ornare metus consectetur. Etiam vel felis malesuada, rutrum elit ut, vestibulum enim. Suspendisse porttitor commodo tellus eget ullamcorper. Praesent lectus massa, fermentum ut sagittis nec, congue mattis risus. Cras pretium dui enim. Nullam scelerisque accumsan sapien et interdum. In id enim tellus. Mauris auctor nulla vel leo euismod, ut sodales ex iaculis. Sed ut tincidunt libero. Vivamus justo odio, mollis sit amet placerat id, finibus pharetra leo.</p></div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MyAbout from '../components/MyAbout'
+import Footer from '../components/Footer'
 
 export default function About() {
     return (
         <div>
             <MyAbout />
+            <Footer />
         </div>
     )
 }
