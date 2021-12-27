9da41021ab361d6fb7b6494586ee20be986cdc06
Ida Delphine <idadelm@gmail.com>
2021-12-27 09:41:26 +0100

    Remove menu from entire app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import './App.css';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './global';
 import { theme } from './theme';
 import Cursor from './components/Cursor';
 import MyRoutes from './routes/MyRoutes';
-import Menu from './components/Menu/Menu';
-import Burger from './components/Burger/Burger';
-import {useOnClickOutside} from './hooks'
+
 
 function App() {
-  const [open, setOpen] = useState(false);
-  const node = useRef();
-  useOnClickOutside(node, () => setOpen(false));
+
   return (
     <ThemeProvider theme={theme}>
       <>
@@ -23,10 +19,6 @@ function App() {
           <MyRoutes />
         </div>
     </>
-    <div ref={node}>
-        <Burger open={open} setOpen={setOpen} />
-        <Menu open={open} setOpen={setOpen} />
-    </div>
     </ThemeProvider>
   );
 }
