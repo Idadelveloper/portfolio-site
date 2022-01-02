e1cb2a884077fe32a1c617ec43decbe0f0576616
Ida Delphine <idadelm@gmail.com>
2022-01-02 18:44:18 +0100

    Add 404 page routes

--- client/src/routes/MyRoutes.js
+++ client/src/routes/MyRoutes.js
@@ -1,11 +1,12 @@
 import React, {Fragment} from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Projects from '../pages/Projects';
 import Resume from '../pages/Resume';
 import Blog from '../pages/Blog';
 import Contact from '../pages/Contact';
+import NotFoundPage from '../pages/NotFoundPage';
 
 export default function MyRoutes() {
     return (
@@ -17,6 +18,8 @@ export default function MyRoutes() {
                 <Route path="/blog" element={<Blog />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route exact path="/" element={<Home />} />
+                <Route path="*" element={<NotFoundPage />} />
+                <Route path="*" element={<Navigate to ="*" />}/>
             </Routes>
         </Router>
     )
