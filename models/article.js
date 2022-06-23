ef2d804c4ef4520f49d6f20746529785240a5335
Ida Delphine <idadelm@gmail.com>
2022-06-23 11:41:38 +0100

    Setup blog routes and mongodb database

---
+++ models/article.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    markdown: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    sanitizedHtml: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Article', articleSchema)
--- server.js
+++ server.js
@@ -2,31 +2,48 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
+const Article = require('./models/article')
+const dotenv = require("dotenv")
 
 // initialize app
 const app = express();
 const PORT = process.env.PORT || 8000;
+dotenv.config()
+
+// connect to mongodb
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@portfoliodb.n5g0z.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/idadelveloper', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then((result) => app.listen(PORT, () => {
+        console.log(`Server is starting at ${PORT}`);
+    }))
+    .catch((err) => console.log(err));
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!');
+})
+
 
 // Log http requests
 app.use(morgan('tiny'));
 
 // app routes
-app.get('/api', (req, res) => {
-    const data = {
-        username: 'Idadel',
-        age: 22
-    }
-    res.json(data)
+app.get('/articles', async(req, res) => {
+
+    const articles = await Article.find()
+
+    res.json(articles)
+
 })
 
-app.get('/api/name', (req, res) => {
-    const data = {
-        username: 'Idadelphine',
-        age: 22
-    }
-    res.json(data)
+app.get('/articles/:id', async(req, res) => {
+    const article = await Article.findById(req.params.id);
+
+    res.json(article)
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is starting at ${PORT}`)
-})
+// app.listen(PORT, () => {
+//     console.log(`Server is starting at ${PORT}`)
+// })
