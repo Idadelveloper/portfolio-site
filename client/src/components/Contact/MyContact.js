751a765faa1a4e7dfe60b14e87084ffca0b480b3
Ida Delphine <idadelm@gmail.com>
2022-07-02 21:00:20 +0100

    Set up the contact page to send emails

--- client/src/components/Contact/MyContact.js
+++ client/src/components/Contact/MyContact.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './MyContact.css'
 import contactImage from '../../assets/3d-center-purple.png'
+import emailjs from '@emailjs/browser';
+import { useAlert } from 'react-alert'
 
 export default function MyContact() {
+    const[name, setName] = useState("");
+    const[subject, setSubject] = useState("");
+    const[email, setEmail] = useState("");
+    const[message, setMessage] = useState("");
+
+    const form = useRef();
+    const alert = useAlert();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        setName("");
+        setSubject("");
+        setEmail("");
+        setMessage("");
+
+        emailjs.sendForm(process.env.REACT_APP_EMAIL_SERVICE_ID, process.env.REACT_APP_EMAIL_TEMPLATE_ID, form.current, process.env.REACT_APP_EMAIL_PUBLIC_KEY)
+        .then((result) => {
+            alert.show('Sucess!!, your message was sent.')
+        }, (error) => {
+            alert.show('Opps! Your message was not sent')
+        });
+    };
     return (
         <div>
             <div className="">
@@ -10,23 +34,19 @@ export default function MyContact() {
                     <div className="contact-circle1"></div>
                     <div className="contact-circle2"></div>
                     <div className="contact-form">
-                        <form action="" className="form">
+                        <form action="" className="form" ref={form} onSubmit={sendEmail}>
                             <p className="mt-5">Do you have anything to tell me? I will love to hear from you :)</p>
-                            <div className="name">
-                                {/* <label htmlFor="firstName">First Name</label> */}
-                                <input type="text" name="firstName" className="firstName mt-4" placeholder="First Name" tabIndex="1" required />
+                            
+                            <input type="text" name="name" className="name mt-4" placeholder="Full Name" tabIndex="1" defaultValue={name} onChange={(e) => setName(e.target.value)} required />
 
-                                {/* <label htmlFor="lastName">Last Name</label> */}
-                                <input type="text" name="lastName" className="lastName" placeholder="Last Name" tabIndex="2" />
+                            <input type="text" name="subject" className="subject" placeholder="Subject" tabIndex="2" defaultValue={subject} onChange={(e) => setSubject(e.target.value)} required />
 
-                                {/* <label htmlFor="email">Email</label> */}
-                                <input type="email" name="email" id="email" className="email" placeholder="Email" tabIndex="3" required />
+                            <input type="email" name="email" id="email" className="email" placeholder="Email" tabIndex="3" defaultValue={email} onChange={(e) => setEmail(e.target.value)} required />
 
-                                {/* <label htmlFor="message">Message</label> */}
-                                <textarea name="message" id="message" placeholder="Message" cols="30" rows="10" required></textarea>
+                            <textarea name="message" id="message" placeholder="Message" cols="30" rows="10" defaultValue={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
 
-                                <button type="submit" className="send mb-4">Send</button>
-                            </div>
+                            <button type="submit" className="send mb-4">Send</button>
+                            
                         </form>
                         <div className="contact-image">
                             <img src={contactImage} alt="" srcset="" />
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
+// alert options
+const options = {
+  position: positions.TOP_RIGHT,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
