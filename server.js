ea4a86c6c9d84a41e238e28dd79162b0ef561470
Ida Delphine <idadelm@gmail.com>
2022-07-02 21:07:53 +0100

    Cleaned up server code

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const path = require('path');
 const Article = require('./models/article');
 const dotenv = require("dotenv");
-const querystring = require('querystring');
 
 // initialize app
 const app = express();
@@ -32,60 +31,12 @@ app.use(morgan('tiny'));
 
 // app routes
 app.get('/articles', async(req, res) => {
-    // let query = {}
-    // if (req.query.category) {
-    //     query.category = req.query.category
-    // }
-    // if (req.query.keyword) {
-    //     query.$or = [
-    //         { "title": { $regex: req.query.keyword, $options: 'i' } },
-    //         { "description": { $regex: req.query.keyword, $options: 'i' } }
-    //     ]
-    // }
-    // const articles = await Article.find(query)
-    //     .populate('category')
-    //     .skip(0)
-    //     .limit(9)
-    //     .sort({ createdAt: 'desc' })
-
-
-    // const page = parseInt(req.query.page)
-    // const limit = parseInt(req.query.limit)
-
-    // const startIndex = (page - 1) * limit
-    // const endIndex = page * limit
-
-    // const articles = await Article.find().sort({ createdAt: 'desc' })
-
-    // const results = {}
-
-    // if (endIndex < articles.length) {
-    //     results.next = {
-    //         page: page + 1,
-    //         limit: limit
-    //     }
-    // }
-
-    // if (startIndex > 0) {
-    //     results.previous = {
-    //         page: page - 1,
-    //         limit: limit
-    //     }
-    // }
-
-    // results.results = articles.slice(startIndex, endIndex)
-
     const articles = await Article.find().sort({ createdAt: 'desc' })
-
-
     res.json(articles)
-
 })
 
 app.get('/articles/:slug', async(req, res) => {
-
     const slug = req.params.slug
-
     const article = await Article.findOne({ slug: slug })
 
     res.json(article)
