9c7f5e2c313c34e79e22c9970577fa2497c1e732
Ida Delphine <idadelm@gmail.com>
2022-07-03 09:18:47 +0100

    Made 404 page responsive

--- client/src/components/Error/NotFound.js
+++ client/src/components/Error/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './NotFound.css'
+import {useNavigate} from 'react-router-dom';
 
 export default function NotFound() {
+    const navigate = useNavigate();
     return (
         <div>
-            <div className="not-found">
+            <div className="not-found relative">
                 <div className="not-found-circle1 absolute"></div>
                 <div className="not-found-circle2 absolute"></div>
                 <div className="not-found-circle3 absolute"></div>
@@ -13,7 +15,7 @@ export default function NotFound() {
                         <h1 className="oh-shoot">Oh Shoot!</h1>
                         <p className="message1-404 mb-5">Sorry! We had to let this page go.</p>
                         <p className="message2-404 mb-10">Let's go back, shall we?</p>
-                        <button className="go-back">Let's go back</button>
+                        <button onClick={() => navigate(-1)} className="go-back">Let's go back</button>
                     </div>
                     <div className="fof">
                         <h1 className="four-o-four">404</h1>
--- client/src/pages/NotFoundPage.js
+++ client/src/pages/NotFoundPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import NotFound from '../components/Error/NotFound'
+import Footer from '../components/Footer/Footer'
 
 export default function NotFoundPage() {
     return (
         <div>
             <Navbar title="" />
             <NotFound />
+            <Footer />
         </div>
     )
 }
