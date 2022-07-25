330573882f60982d378a89c4067690d46dd03b61
Ida Delphine <idadelm@gmail.com>
2022-07-25 19:36:59 +0100

    fix blog bug

--- client/src/components/Blog/BlogHome.js
+++ client/src/components/Blog/BlogHome.js
@@ -37,24 +37,24 @@ export default function BlogHome() {
 
         return posts.map((post, index) =>  (
             <motion.div layout key={index} className="w-full max-w-md mx-auto rounded-3xl overflow-hidden shadow-lg mb-20">
-                <img className="w-full card-image" src={post.image} alt="Image for this post" onError={({ currentTarget }) => {
+                <img className="w-full card-image" src={post?.image} alt="Image for this post" onError={({ currentTarget }) => {
                     currentTarget.onerror = null;
                     currentTarget.src=image;
                 }} />
                 <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2 blog-title">{post.title}</div>
+                    <div className="font-bold text-xl mb-2 blog-title">{post?.title}</div>
                     <p className="text-gray-700 text-base">
-                    {post.description}
+                    {post?.description}
                     </p>
                 </div>
                 <div className="flex justify-between px-10">
-                    <p className="text-gray-700 mb-2 text-xs font-semibold italic">Created at {new Date(post.createdAt).toLocaleDateString()}</p>
-                    <p className="text-gray-700 mb-2 text-xs font-semibold italic">Updated at {new Date(post.updatedAt).toLocaleDateString()}</p>
+                    <p className="text-gray-700 mb-2 text-xs font-semibold italic">Created at {new Date(post?.createdAt).toLocaleDateString()}</p>
+                    <p className="text-gray-700 mb-2 text-xs font-semibold italic">Updated at {new Date(post?.updatedAt).toLocaleDateString()}</p>
                 </div>
                 <div className="px-6 pt-4 pb-2 flex justify-between">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">{post.category}</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">{post?.category}</span>
                     
-                    <Link to={post.slug} className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 text-sm rounded-full mb-2 card-read-more flex items-center">Read more</Link>
+                    <Link to={post?.slug} className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 text-sm rounded-full mb-2 card-read-more flex items-center">Read more</Link>
                 </div>
             </motion.div>
             )
